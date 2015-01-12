845917792553baaa53efc57474362948e2748690
690312856 <690312856@qq.com>
2015-01-12 17:31:58 +0800

    算法库

---
+++ src/algorithm/Main.java
@@ -0,0 +1,7 @@
+package algorithm;
+
+public class Main {
+public static void main(String[] arg){
+	System.out.println("one,two");
+}
+}
---
+++ src/solution/Solution.java
@@ -0,0 +1,8 @@
+package solution;
+
+public class Solution {
+	public int singleNumber(int[] A) {
+		int result = A[0];
+		return result;
+	}
+}

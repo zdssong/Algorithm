e6ecc5368a868a5c423ba7cae8ba1bac18c3b6e3
690312856 <690312856@qq.com>
2015-01-23 16:26:47 +0800

    LRU算法还在解决中

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,18 +1,14 @@
 package algorithm;
 
+import solution.LRUCache;
 import solution.Solution;
 
 public class Main {
 
 	public static void main(String[] arg) {
-		Solution solution = new Solution();
-		int[] one = new int[2];
-		one[0] = 1;
-		one[1] = 2;
-		change(one);
-		solution.singleNumber(one);
-		// one[0]+=one[1]++;
-		System.out.println(one[0]++);
+		LRUCache lru = new LRUCache(1);
+		lru.set(2, 1);
+		lru.get(1);
 	}
 
 	public static void change(int[] one) {
--- src/solution/LRUCache.java
+++ src/solution/LRUCache.java
@@ -1,7 +1,8 @@
 package solution;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Design and implement a data structure for Least Recently Used (LRU) cache. It
@@ -17,17 +18,56 @@ import java.util.Map;
 public class LRUCache {
 
 	private int capacity;
-	private HashMap<Integer, Integer> map = new HashMap<>();
+	private LinkedHashMap<Integer, Integer> map;
+	
+	private List<Integer> list = new ArrayList<Integer>();
 
 	public LRUCache(int capacity) {
 		this.capacity = capacity;
+		map = new LinkedHashMap<Integer, Integer>(capacity,0.75f, true);
 	}
 
 	public int get(int key) {
-		return 0;
+		if (map.containsKey(key)) {
+			return map.get(key);
+		} else {
+			return -1;
+		}
 	}
 
 	public void set(int key, int value) {
-
+		if (map.containsKey(key)) {
+			map.put(key, value);
+		} else if (map.size() < capacity) {
+			map.put(key, value);
+		} else {
+			map.remove(list.get(capacity - 1));
+		}
 	}
+
+	/**
+	 * 编译不通过，因为当capacity很大的时候，超过了时间限制
+	 * */
+	private void changeOrderUseList(int key) {
+		int position = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == key)
+				position = i;
+		}
+		for (int i = position; i >= 1; i--) {
+			list.set(i, list.get(i - 1));
+		}
+		list.set(0, key);
+	}
+
+	/**
+	 * 经过了优化，但是在编译时还是超时了
+	 * */
+	private void changeOrderWithPositionUseList(int key, int position) {
+		for (int i = position; i >= 1; i--) {
+			list.set(i, list.get(i - 1));
+		}
+		list.set(0, key);
+	}
+
 }
